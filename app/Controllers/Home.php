85b5f8d4edcbcf288525c10276092241bd788495
Kirimito <ditoachanation@gmail.com>
2022-09-24 19:56:04 +0700

    awasd

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -32,9 +32,11 @@ class Home extends BaseController
       $name = str_replace(array("\r\n", "\r", "\n", "\t",""), '', $namev);
       $imgprvs = $shtml->find('ul.cmn-list-product03 img', $i)->src;
       $imgprv = str_replace(array("https://",""), 'http://', $imgprvs);
+      $linkact = "";
       $aktris[] = [
         'name' => $name,
         'imgprview' => $imgprvs,
+        'linkact' => $linkact,
       ];
 
       $img = file_get_contents($imgprvs, true, $context);
