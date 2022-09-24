6e05009dc9992a025ecfbad3965d3dfb60f3c71c
Kirimito <ditoachanation@gmail.com>
2022-09-24 19:18:04 +0700

    ghfhg

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,20 +11,20 @@ class Home extends BaseController
     return view('hterm');
   }
   public function actress(){
-    $options = array(
-      'http'=>array(
-        'method'=>"GET",
-        'header'=>"Accept-language: en\r\n" .
-                  "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
-                  "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
-      )
-    );
-    $context = stream_context_create($options);
     $client = new httpClient();
     $destdir = 'imgprv/';
     $res = $client->request('GET', 'https://www.r18.com/videos/vod/movies/actress/?page=2');
     $shtml = str_get_html($res->getBody());
     for ($i = 0; $i < 30; $i++) {
+      $options = array(
+        'http'=>array(
+          'method'=>"GET",
+          'header'=>"Accept-language: en\r\n" .
+                    "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+                    "User-Agent: Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n" // i.e. An iPad 
+        )
+      );
+      $context = stream_context_create($options);
       $namev = $shtml->find('div.txt01', $i)->plaintext;
       $name = str_replace(array("\r\n", "\r", "\n", "\t",""), '', $namev);
       $imgprvs = $shtml->find('ul.cmn-list-product03 img', $i)->src;
@@ -34,7 +34,7 @@ class Home extends BaseController
         'imgprview' => $imgprvs,
       ];
 
-      $img = file_get_contents($imgprvs);
+      $img = file_get_contents($imgprvs, false, $context);
       file_put_contents($destdir.substr($imgprv, strrpos($imgprv,'/')), $img);
     }
     $fxf[] = [
