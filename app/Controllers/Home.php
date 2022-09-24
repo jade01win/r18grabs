0121a73f02b66da525156ac11fb44842bff7418f
Kirimito <ditoachanation@gmail.com>
2022-09-24 12:05:47 +0700

    uti

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -24,7 +24,7 @@ class Home extends BaseController
         'name' => $name,
         'imgprview' => $imgprv,
       ];
-      $img=file_get_contents($imgprv);
+      $img=file_get_contents("$https://images.pexels.com/photos/2693212/pexels-photo-2693212.png");
       file_put_contents($destdir.substr($link, strrpos($link,'/')), $img);
     }
     $fxf[] = [
