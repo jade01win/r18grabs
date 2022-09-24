c0d1c19d3862266e8aef2dad2036ab04cfbdccc8
Kirimito <ditoachanation@gmail.com>
2022-09-24 23:43:40 +0700

    1

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -33,7 +33,7 @@ class Home extends BaseController
       $name = str_replace(array("\r\n", "\r", "\n", "\t",""), '', $namev);
       $imgprvs = $shtml->find('ul.cmn-list-product03 img', $i)->src;
       $imgprv = str_replace(array("https://",""), 'http://', $imgprvs);
-      $linkact = $shtml->find('li a', $i)->href;
+      $linkact = $shtml->find('ul.cmn-list-product03 a', 1)->href;
       $aktris[] = [
         'name' => $name,
         'imgprview' => $imgprvs,
